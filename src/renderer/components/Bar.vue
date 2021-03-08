03e58e79de11a09b626b920eca4e7803018fe9c8
yang <oseau@me.com>
2021-03-09 04:56:30 +0800

    add transition to score bar

--- src/renderer/components/Bar.vue
+++ src/renderer/components/Bar.vue
@@ -1,6 +1,17 @@
 <template>
+  <Modal v-if="showModal">
+    <template v-slot:body>
+      <textarea rows="4" v-model="nameEdit"></textarea>
+      <button @click.prevent="clickOK">ok</button>
+    </template>
+  </Modal>
   <li class="score-item">
-    <div class="weui-progress">
+    <div class="name" @click.prevent="showModal = true">{{ name }}</div>
+    <div
+      class="weui-progress"
+      @click.prevent="$emit('clickBar', idx, 1)"
+      @click.right.prevent="$emit('clickBar', idx, -1)"
+    >
       <div class="weui-progress__bar">
         <div
           class="weui-progress__inner-bar js_progress"
@@ -13,19 +24,41 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import Modal from "./Modal.vue";
+
 export default {
   name: "Bar",
+  emits: ["clickBar", "editName"],
+  components: { Modal },
   props: {
+    name: String,
+    idx: Number,
     percent: Number,
     score: Number,
+  },
+  setup(props, { emit }) {
+    const showModal = ref(false);
+    const nameEdit = ref(props.name);
+    const clickOK = () => {
+      showModal.value = false;
+      emit("editName", props.idx, nameEdit.value);
+    };
+    return { showModal, clickOK, nameEdit };
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.name {
+  margin-bottom: 1rem;
+  line-height: normal;
+  word-break: keep-all;
+}
 .score-number {
   width: 4rem;
+  font-size: xx-large;
+  font-weight: 900;
 }
 .score-item {
   margin: 5rem 0;
@@ -37,14 +70,19 @@ export default {
 }
 .weui-progress__bar {
   background-color: #ededed;
-  height: 1rem;
+  height: 2rem;
   -webkit-box-flex: 1;
   flex: 1;
 }
-
 .weui-progress__inner-bar {
   width: 0;
   height: 100%;
   background-color: #07c160;
+  transition: 1s all linear;
+}
+
+textarea {
+  width: 100%;
+  height: auto;
 }
 </style>
--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,16 +1,68 @@
 <template>
-  <button @click="count++">count is: {{ count }}</button>
+  <ul class="score-list">
+    <Bar
+      v-for="(score, idx) in scores"
+      @clickBar="clickBar"
+      @editName="editName"
+      :idx="idx"
+      :name="score.name"
+      :percent="score.percent"
+      :score="score.score"
+    />
+  </ul>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Bar from "./Bar.vue";
 
-const count = ref(0);
+const scores = ref([
+  {
+    name: "黄柄淇、高明泽、王锦程、孔森羽",
+    score: 10,
+    percent: 100,
+  },
+  {
+    name: "孔荣欣、薛皓太、王一博、齐林、王浩翔、潘昱龙、王子康",
+    score: 10,
+    percent: 100,
+  },
+  { name: "邵熙杰、张宝聚、高小珺、王思彤", score: 10, percent: 100 },
+]);
+
+const clickBar = (idx, diff) => {
+  scores.value[idx].score += diff;
+  let smallest, largest;
+  for (const [idx, score] of scores.value.entries()) {
+    if (typeof smallest === "undefined") {
+      smallest = { idx, score: score.score };
+    }
+    if (typeof largest === "undefined") {
+      largest = { idx, score: score.score };
+    }
+    if (score.score < smallest.score) {
+      smallest = { idx, score: score.score };
+    }
+    if (score.score > largest.score) {
+      largest = { idx, score: score.score };
+    }
+  }
+  const diffMax = largest.score - smallest.score;
+  for (const [idx, score] of scores.value.entries()) {
+    scores.value[idx].percent = parseInt(
+      ((score.score ** 2 / largest.score ** 2) * 100).toFixed()
+    );
+  }
+};
+
+const editName = (idx, name) => {
+  scores.value[idx].name = name;
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
-  color: #42b983;
+.score-list {
+  margin: 0 2rem;
 }
 </style>
